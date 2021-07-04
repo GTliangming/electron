4f84ef3be13bf2c1c5c03693db7818ad1394499d
liangming1 <liangming1@lightinthebox.com>
2021-07-04 23:43:38 +0800

    开发版本 fix1

--- start.js
+++ start.js
@@ -1,13 +1,13 @@
-const webpack = require("webpack");
-const path = require("path");
-const { exec } = require("child_process");
+// const webpack = require("webpack");
+// const path = require("path");
+// const { exec } = require("child_process");
 
-webpack(require("./webpack.main.config"), (err, stats) => {
-	if (err) {
-		console.error(err);
-	} else if (stats.hasErrors()) {
-		console.error(stats.toJson().errors.join("\n"));
-	} else {
-		exec("electron .");
-	}
-});
+// webpack(require("./webpack.main.config"), (err, stats) => {
+// 	if (err) {
+// 		console.error(err);
+// 	} else if (stats.hasErrors()) {
+// 		console.error(stats.toJson().errors.join("\n"));
+// 	} else {
+// 		exec("electron .");
+// 	}
+// });
