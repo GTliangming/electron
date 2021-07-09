716deac41247746ae41c523d15adbafc03f10026
liangming1 <liangming1@lightinthebox.com>
2021-07-09 14:50:50 +0800

    更新打包

--- main-process/main.js
+++ main-process/main.js
@@ -1,87 +1,73 @@
-const electron = require('electron');
-// Module to control application life.
-const app = electron.app;
-// Module to create native browser window.
-const BrowserWindow = electron.BrowserWindow;
+// 引入electron并创建一个Browserwindow
+const { app, BrowserWindow } = require('electron')
+const path = require('path')
+const url = require('url')
 
-const path = require('path');
-const url = require('url');
+// 保持window对象的全局引用,避免JavaScript对象被垃圾回收时,窗口被自动关闭.
+let mainWindow
 
-// Keep a global reference of the window object, if you don't, the window will
-// be closed automatically when the JavaScript object is garbage collected.
-let mainWindow;
-
-global.sharedObject = {
-  topId: null,
-  topWebContentsId: null,
-};
-
-function createMainWindow() {
-  // Create the browser window.
-  mainWindow = new BrowserWindow({
-    width: 1024,
-    height: 768,
+function createWindow() {
+  //创建浏览器窗口,宽高自定义具体大小你开心就好
+  mainWindow = new BrowserWindow({ 
+    width: 800, 
+    height: 600,
     webPreferences: {
       nodeIntegration: true,
       enableRemoteModule: true 
     }
-  });
+  })
 
-  // and load the index.html of the app.
-  mainWindow.loadURL(url.format({
-    pathname: path.join(__dirname, '../index.html'),
-    protocol: 'file:',
-    slashes: true
-  }));
+  // 加载应用----适用于 react 项目
+  // mainWindow.loadURL('http://localhost:3000/');
 
-  // Open the DevTools.
-  // mainWindow.webContents.openDevTools()
 
-  // Emitted when the window is closed.
+  mainWindow.loadFile(path.join(__dirname, '../index.html'))
+
+  // mainWindow.loadURL(url.format({
+  //   pathname: path.join(__dirname, '../index.html'),
+  //   protocol: 'file:',
+  //   slashes: true
+  // }));
+
+  // 打开开发者工具，默认不打开
+  mainWindow.webContents.openDevTools()
+
+  // 关闭window时触发下列事件.
   mainWindow.on('closed', function () {
-    // Dereference the window object, usually you would store windows
-    // in an array if your app supports multi windows, this is the time
-    // when you should delete the corresponding element.
     mainWindow = null;
     app.quit();
-  });
-
-  mainWindow.on("focus", () => {
-    const topId = global.sharedObject.topId;
-    if (topId) {
-      BrowserWindow.fromId(topId).setAlwaysOnTop(false);
-    }
-  });
-  mainWindow.on("blur", () => {
-    const topId = global.sharedObject.topId;
-    if (topId) {
-      BrowserWindow.fromId(topId).setAlwaysOnTop(true);
-    }
   })
 }
 
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
-app.on('ready', createMainWindow);
+// 当 Electron 完成初始化并准备创建浏览器窗口时调用此方法
+app.on('ready', createWindow)
 
-// Quit when all windows are closed.
+// 所有窗口关闭时退出应用.
 app.on('window-all-closed', function () {
-  // On OS X it is common for applications and their menu bar
-  // to stay active until the user quits explicitly with Cmd + Q
+  // macOS中除非用户按下 `Cmd + Q` 显式退出,否则应用与菜单栏始终处于活动状态.
   if (process.platform !== 'darwin') {
-    app.quit();
+    app.quit()
   }
-});
+})
 
 app.on('activate', function () {
-  // On OS X it's common to re-create a window in the app when the
-  // dock icon is clicked and there are no other windows open.
+  // macOS中点击Dock图标时没有已打开的其余应用窗口时,则通常在应用中重建一个窗口
   if (mainWindow === null) {
-    createMainWindow();
+    createWindow()
   }
-});
+})
+
+// 你可以在这个脚本中续写或者使用require引入独立的js文件.   
+
+
+
+
+
+
 
 // In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and require them here.
 require("./api");
+
+
+
